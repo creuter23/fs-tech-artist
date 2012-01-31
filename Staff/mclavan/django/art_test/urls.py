b2e7a4beeebe573f8ef8e21f7e3ff70fb9bf0d99
Heith Seewald <hseewald@fullsail.com>
2012-01-31 18:37:42 -0500

    updated style

--- Staff/mclavan/django/art_test/urls.py
+++ Staff/mclavan/django/art_test/urls.py
@@ -17,12 +17,4 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-<<<<<<< HEAD
-    # url(r'^admin/', include(admin.site.urls)),
 )
-=======
-     #url(r'^admin/', include(admin.site.urls)),
-)
->>>>>>> 3fb28d8e52d65069086407ac5cf7cc9cdb371422
