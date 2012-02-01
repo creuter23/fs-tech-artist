403a1c03e72d59a584dfa632b104fb767952d1ee
Kenneth Dietrich <kennydietrich@gmail.com>
2012-02-01 10:46:26 -0500

    Fixed the current render type button, it finally works!

--- Intern/Dietrich_Kenneth/waterSprinklerTutorial_kd.py
+++ Intern/Dietrich_Kenneth/waterSprinklerTutorial_kd.py
@@ -1,7 +1,7 @@
 import maya.cmds as cmds
 
 # Creating emitter and particle object
-emit = cmds.emitter(n='spray',) # Returns lists
+emit = cmds.emitter(n='spray', dx=0, dy=1, dz=0, r=100, sp=0.17, spd=5) # Returns lists
 
 # Return List [particle, particleShape]
 part = cmds.particle(n='droplets')
@@ -16,6 +16,22 @@ hose_objects = cmds.ls(sl=True)
 cmds.parent( emit[0], hose_objects[0] )
 
 part_attrs = {'depthSort':True, 'particleRenderType':0}
+
+for attr, value in part_attrs.items():
+    cmds.setAttr('%s.%s' %(part[1], attr), value)
+   
+#The code for Current Render Type button inside the particleShape
+#ln=longName, at=attributeType, dv=defaultValue, min=minValue, max=maxValue
+#When activating the Current Render Type button, make sure the code is part[1], and not [0]:
+#[0] is the emitter.
+cmds.addAttr(part[1], internalSet=True, ln="colorAccum", at="bool", dv=False )
+cmds.addAttr(part[1], internalSet=True, ln="useLighting", at="bool", dv=False )
+cmds.addAttr(part[1], internalSet=True, ln="pointSize", at="long", min=1, max=60, dv=2 )
+cmds.addAttr(part[1], internalSet=True, ln="multiCount", at="long", min=1, max=60, dv=10 )
+cmds.addAttr(part[1], internalSet=True, ln="multiRadius", at="float", min=0, max=10, dv=0.3 )
+cmds.addAttr(part[1], internalSet=True, ln="normalDir", at="long", min=1, max=3, dv=2 )
+
+part_attrs = {'colorAccum':False, 'useLighting':True, 'particleRenderType':0,}
 
 for attr, value in part_attrs.items():
     cmds.setAttr('%s.%s' %(part[1], attr), value)
